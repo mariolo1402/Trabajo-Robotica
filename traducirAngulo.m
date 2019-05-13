1f3be64c19d78bcfe5eb06c96caf95300e021d6e
mariolo1402 <38990353+mariolo1402@users.noreply.github.com>
2019-05-13 11:47:28 +0200

    Add files via upload

--- traducirAngulo.m
+++ traducirAngulo.m
@@ -1,31 +1,37 @@
-function q3=traducirAngulo(q, alpha)
-
-%q es el angulo que queremos fijar en el codo. Por lo que nuestro angulo
-%será:
-q1=pi-q;
-%Lo primero es cambiar las longitudes de las barras por las reales de
+function betha=traducirAngulo(delta, alpha)
+%Lo primero es introducir las longitudes de las barras reales de
 %nuestro diseño.
-L1=1;%L1 es la distancia del eje del codo al eje con la barra paralela que tira
-L2=1;%L2 es la longitud entre ejes de la barra paralela
-L3=1;%L3 es la longitud entre ejes de la barrita que va al servo.
-L4=1;%L4 es la distancia entre ejes del eslabon principal
-L5=1;L6=1; %distancias horizontal y vertical entre ejes de los servos
-L=1; %distancia del brazo principal
+L1=0.055; 
+L2=0.180;
+L3=0.050;
+L6=0.100;L5=0.006; 
+L=0.180; 
+
+%Calculamos la longitud del vector CA, que aunque es ficticio, define el
+%eslabon fijo del problema y depende de el angulo alpha.
+L4=sqrt((-L6-L*cos(alpha))^2+(L5+L*sin(alpha))^2);
+
+%Calculamos el angulo que formann los vector CE y CA y el angulo gamma:
+
+CE=[-L6; L5];
+CA=[-L6-L*cos(alpha); L5+L*sin(alpha)];
+landa=acos((CE'*CA)/(norm(CE)*norm(CA)));
+gamma=atan(L5/L6);
+
+%Para lograr tener un angulo delta en el codo necesitaremos el sigiente theta1 (q1):
+
+q2=alpha-gamma-landa;
+
+q1=pi-delta-q2;
+
 %Implementamos la expresion analitica de un mecanismo de 4 eslabones, 1 fijo: 
 
-c1=L4/L1; c3=L4/l3; c4=(L1^2-L2^2+L3^2+L4^2)/(2*L1*L3);
+c1=L4/L1; c3=L4/L3; c4=(L1^2-L2^2+L3^2+L4^2)/(2*L1*L3);
 
-p=[(c1+c4+cos(q1)*(-1-c3)) -2*sin(q1) (-c1+c4+cos(-q1)*(1-c3))];
-x=root(p);
-y=atan(2*x);
+p=[(c1+c4+cos(q1)*(-1-c3)) -2*sin(q1) (-c1+c4+cos(q1)*(1-c3))];
+x=roots(p);
+q3=abs(2*atan(x));
 
-%calculamos el angulo que forma el vector entre ejes de los servos y el eje
-%del servo2 con el eje del codo:
-
-vector1=[-L5;L6];
-vector2=[-L5+L*cos(alpha);L6+L*sin(alpha)];
-betha=acos((vector1'*vector2)/(norm(vector1)*norm(vector2)));
-
-%devolvemos el angulo que debe tener el servo:
-q3=pi-y-betha;
+%Devolvemos el angulo que debe tener el servo:
+betha=(pi-gamma-landa-q3)*180/pi;
 end
